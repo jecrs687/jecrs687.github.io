e5cb3f14c7740e4caae54457188f21761f812b84
jecrs687 <emanuelcascone@gmail.com>
2020-01-18 13:13:08 +0100

    Initial commit

---
+++ precache-manifest.80b41f9418c645c63c6e55fcf5120367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7407cfeeb2e1489e80718a125a1cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbed10c32eea30220114",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "97596c4fb51373e91272",
+    "url": "/static/js/2.88c219eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.88c219eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbed10c32eea30220114",
+    "url": "/static/js/main.e6145b79.chunk.js"
+  },
+  {
+    "revision": "6a38750576091ce5575a",
+    "url": "/static/js/runtime-main.c0916296.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
