c43fb4891897360a363d95bec240a7b2771fb0b2
José Emanuel <45917859+jecrs687@users.noreply.github.com>
2020-02-10 01:16:43 +0100

    Add files via upload

---
+++ precache-manifest.4c72a0032c0cf7cb5b0b328fbd7ed701.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d0ed24490eff1e74833af42a2d6346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7652cdee7f1b621d4a94",
+    "url": "/static/css/main.eccbd80b.chunk.css"
+  },
+  {
+    "revision": "d54ced6df21214039b86",
+    "url": "/static/js/2.d08a5cbf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d08a5cbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7652cdee7f1b621d4a94",
+    "url": "/static/js/main.bbb0ff8b.chunk.js"
+  },
+  {
+    "revision": "6a38750576091ce5575a",
+    "url": "/static/js/runtime-main.c0916296.js"
+  },
+  {
+    "revision": "13f79d1fb92f130327434f44b966716d",
+    "url": "/static/media/background.13f79d1f.jpg"
+  },
+  {
+    "revision": "de864e57031fcb39acdc01a982bfe64f",
+    "url": "/static/media/jecrs687.de864e57.jpg"
+  }
+]);
