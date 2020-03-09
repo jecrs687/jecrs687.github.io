722f30df161fe94381aba9475b5ff1fe18d74fd4
jecrs687 <emanuelcascone@gmail.com>
2020-03-09 11:26:00 -0300

    small updates
    --add projects in coment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,15 +3,13 @@ import "./Projects.css"
 
 
 
-function Project({colors,name}){
-    
+function Project({colors,name,link}){
     var palette = Math.floor(Math.random() * colors.length);
-
     var color= colors[palette].primary;
     var color2= colors[palette].secundary;
     var color3= colors[palette].third;
 return(
-    <div className='project' onClick={()=>{}} style={{background: `linear-gradient(${color}, ${color2}, ${color3})`}}>
+    <div className='project' onClick={()=>{window.open(`https://jecrs687.github.io${link}`)}} style={{background: `linear-gradient(${color}, ${color2}, ${color3})`}}>
         <span className='title'>{name}</span>
     </div>
 )
@@ -22,7 +20,12 @@ export function Projects({data}){
         <div className='projects'>
               {Object.keys(data.projects).map(
               value=>(
-              <Project colors={data.paletter} name={value} data={data.projects[value]}/>
+                  <div className="box-project">
+                      <span className="title-project">{value}</span>
+                        <div className="grid-projects">
+                          {data.projects[value].map((value)=>(<Project colors={data.paletter} name={value.name} link={value.link} />))}
+                        </div>
+                    </div>
               ))}          
         </div>
     )
