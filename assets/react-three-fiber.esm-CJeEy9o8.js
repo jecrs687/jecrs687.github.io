b9c8940e9de84cf05fdf3c408713016c422d7a63
jecrs687 <emanuelcascone@gmail.com>
2025-04-10 10:51:01 +0200

    Updates

--- assets/react-three-fiber.esm-CJeEy9o8.js
+++ assets/react-three-fiber.esm-CJeEy9o8.js
@@ -1,4 +1,4 @@
-import{r as Ke,w as hx,x as fx,j as xi}from"./index-DAk0qmfc.js";/**
+import{r as Ke,w as hx,x as fx,j as xi}from"./index-BgjIyfL4.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
